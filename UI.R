ea5b06bc7fef17aafa4ca5182bba13c8282de325
Winston Saunders <winstonasaunders@gmail.com>
2014-12-07 10:42:59 -0800

    update UI

--- UI.R
+++ UI.R
@@ -2,3 +2,23 @@
 ##
 ## The product displays different crime maps of San Francisco for both crime type and by year.
 ##
+
+library(shiny)
+
+shinyUI(
+        pageWithSidebar(
+                # Application title
+                headerPanel("Crime Mapping"),
+                sidebarPanel(
+                        numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
+                        submitButton('Submit')
+                ),
+                mainPanel(
+                        h3('Results of prediction'),
+                        h4('You entered'),
+                        verbatimTextOutput("inputValue"),
+                        h4('Which resulted in a prediction of '),
+                        verbatimTextOutput("prediction")
+                )
+        )
+)
