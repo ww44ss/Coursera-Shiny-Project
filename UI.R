e3fb337b76247feb5eef0123c12f878ee96ae628
Winston Saunders <winstonasaunders@gmail.com>
2014-12-07 10:35:28 -0800

    Add UI.R commit

---
+++ UI.R
@@ -0,0 +1,4 @@
+## This is the ui.R for the prject in the Data Products Coursera series on Data Science
+##
+## The product displays different crime maps of San Francisco for both crime type and by year.
+##
