4e0568623eff66854a39e378beb74aa5a8719280
Winston Saunders <winstonasaunders@gmail.com>
2014-12-20 10:57:42 -0800

    small edits

--- UI.R
+++ UI.R
@@ -1,4 +1,4 @@
-## This is the ui.R for the prject in the Data Products Coursera series on Data Science
+## This is the ui.R for the project in the Data Products Coursera series on Data Science
 ##
 ## The product displays different crime maps of San Francisco for both crime type and by year.
 ##
